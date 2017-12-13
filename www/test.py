1760edef8e3512d64bbe835a5271be51ecf0d11e
xiaochenzz <zhongjvzhang@gmail.com>
2017-12-13 21:21:24 +0800

    3

---
+++ www/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+'''
+Models for user,blog.comment.
+'''
+__author__ = 'zzj'
+
+import sys
+import orm
+import asyncio
+from models import User, Blog, Comment
+
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user='www-data',
+                          password='', db='awesome')
+    u = User(name='Test6', email='test6@example.com',
+             passwd='1234567890', image='about:blank')
+
+    await u.save()
+    await orm.destory_pool()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
